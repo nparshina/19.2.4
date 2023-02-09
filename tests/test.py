cb92ede3c6d14756fa925bbda4b81ce2e2fff938
Natalia Parshina <minnie@ecwid.com>
2023-02-09 15:18:44 +0400

    Калькулятор и позитивные тесты для него

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.calc import Calcu
+from app.calc import Calculator
 
 class TestCalc:
     def setup(self):
@@ -9,18 +9,22 @@ class TestCalc:
     def test_adding_success(self):
         assert self.calc.adding(self, 1, 1) == 2
 
+    #тест сложения
+
     def test_subtraction_success(self):
         assert self.calc.subtraction(self, 13, 3) == 10
+
+    #тест вычитания
 
     def test_multiplication_success(self):
         assert self.calc.multiply(self, 5, 6) == 30
 
+    # тест умножения
+
     def test_division_success(self):
         assert self.calc.division(self, 12, 4) == 3
 
-    def test_zero_division(self):
-        with pytest.raises(ZeroDivisionError):
-            self.calc.division(self, 1, 0)
+    # тест деления
 
     def teardown(self):
         print('Выполнение метода Teardown')
